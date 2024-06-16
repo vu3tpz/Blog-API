ceb3663aa53471ba203a576e42a32deb1c60a94f
Vishnu M <vishnuvmk029@gmail.com>
2024-06-16 12:58:51 +0530

    sign up function completed

--- apps/access/serializers/authentication.py
+++ apps/access/serializers/authentication.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from apps.access.models import User
 from apps.common.serializers import AppWriteOnlyModelSerializer
 
@@ -5,6 +7,17 @@ from apps.common.serializers import AppWriteOnlyModelSerializer
 class SignUpSerializer(AppWriteOnlyModelSerializer):
     """Serializer to create User"""
 
+    password = serializers.CharField(write_only=True)
+
     class Meta(AppWriteOnlyModelSerializer.Meta):
         model = User
         fields = ["first_name", "last_name", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
+        )
+        return user
--- apps/access/views/authentication.py
+++ apps/access/views/authentication.py
@@ -1,10 +1,15 @@
-from apps.access.models import User
+from rest_framework import status
+
 from apps.access.serializers import SignUpSerializer
-from apps.common.views import AppModelCreateAPIViewSet, NonAuthenticatedAPIMixin
+from apps.common.views import AppAPIView, NonAuthenticatedAPIMixin
 
 
-class SignUpAPIViewSet(NonAuthenticatedAPIMixin, AppModelCreateAPIViewSet):
+class SignUpAPIViewSet(NonAuthenticatedAPIMixin, AppAPIView):
     """Used to create user in the application if they not exist."""
 
-    serializer_class = SignUpSerializer
-    queryset = User.objects.all()
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return self.send_response(status=status.HTTP_201_CREATED)
+        return self.send_error_response(serializer.errors)
